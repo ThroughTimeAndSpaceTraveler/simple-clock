3e339810406b7a0e034aa030c9449579c2488f03
ThroughTimeAndSpaceTraveler <theelectromagneticinduction@gmail.com>
2021-09-06 15:07:06 +0300

    bug and typo fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-const secondHand = document.querySelector(".s-hand");
-const minsHand = document.querySelector("m-hand")
-const hoursHand = document.querySelector("h-hand")
+const secondHand = document.querySelector('.s-hand');
+const minsHand = document.querySelector('.m-hand');
+const hoursHand = document.querySelector('.h-hand');
 
 function setDate() {
     const now = new Date();
+    
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -13,9 +14,8 @@ function setDate() {
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegree = ((hours / 60) * 360) + 90;
+    const hoursDegree = ((hours / 12) * 360) + 90;
     hoursHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
-
-
+setInterval(setDate, 1000)
